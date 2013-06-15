180e8107fd6828144b32925daab804380802ec0f
cefleet <cefleet@cefleet-Lappy.(none)>
2013-06-15 12:48:06 -0400

    Adding items to views

--- tests/View_test.js
+++ tests/View_test.js
@@ -3,10 +3,19 @@ TestCase("ViewTest", {
 		MCOR.appName = 'paperless';		
 	},
 	
-	"test will show that the main view has been created": function(){
+	"test will show that elements for for the main Init view have been created": function(){
 		var mainView = Paperless.Views.Main.init();
+
+		assertElementId('content', mainView)		
+		assertElementId('navbar',mainView.childNodes[0]);
+		assertElementId('contentBody', mainView.childNodes[1]);
+
+	},
+	"test will show that the navbar elements are created": function(){
+		var navBar = Paperless.Views.Main.navBar();
 		
-		assertNotUndefined(mainView.childNodes[0]);
+		assertElementId('navbar', navBar);
+		assertClassName('navbar-inner', navBar.childNodes[0]);
 		
 	}
 });
